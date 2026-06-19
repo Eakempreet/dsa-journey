8a648a8ec27301940b0b9211b348e12217094638
Eakempreet <eakempreetsinghlotey@gmail.com>
2026-06-19 13:43:21 +0530

    Basic input output code

---
+++ 01-cpp-foundations/basic_In_Out.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  // used for inputs/outputs
+#include <math.h>    // used for mathematical functions
+using namespace std;
+// if "using namespace std;" is not use then we have to use std::
+// std::cout << "Hey there, World!" << std::endl;
+
+int main(){
+    // Output
+    cout << "\nHey there, World!" << endl;
+
+    // Input
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << "\nValue of y: " << y;
+
+    return 0;
+}
