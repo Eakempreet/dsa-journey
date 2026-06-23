ad256c3922d231b6552be00ff2ba4d69daabbfb8
Eakempreet <eakempreetsinghlotey@gmail.com>
2026-06-23 14:37:06 +0530

    if else blocks and their usage

---
+++ 01-cpp-foundations/03_if_else.cpp
@@ -0,0 +1,20 @@
+/*
+#include <iostream>
+using namespace std;
+// Write a program that takes an input as age
+// and prints if you are an adult or not 
+
+int main(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age>=18){
+        cout << "You are adult!" << endl;
+    }
+    else{
+        cout << "You are Not an adult!" << endl;
+    }
+    return 0;
+}
+*/
