4fb0fa07a2fbbadf552730dec2ddb9ed316942f9
Eakempreet <eakempreetsinghlotey@gmail.com>
2026-06-23 15:08:35 +0530

    Multiple if else examples with nested conditions

--- 01-cpp-foundations/03_if_else.cpp
+++ 01-cpp-foundations/03_if_else.cpp
@@ -18,3 +18,83 @@ int main(){
     return 0;
 }
 */
+
+/*
+#include <iostream>
+using namespace std;
+
+
+// A school has following rules for grading system
+// a. Below 25 - F
+// b. 25 to 44 - E
+// c. 44 to 49 - D
+// d. 50 to 59 - C
+// e. 60 to 79 - B
+// f. 80 to 100 - A
+// Ask the user marks and print the corresponding grade.
+
+
+int main(){
+    int marks;
+    cout << "Enter the marks: ";
+    cin >> marks;
+
+    if (marks<25){
+        cout << "Grade: F";
+    }
+    else if (marks >= 24 && marks <= 44){
+        cout << "Grade: E";
+    }
+    else if (marks >= 45 && marks <= 49){
+        cout << "Grade: D";
+    }
+    else if (marks >= 50 && marks <= 59){
+        cout << "Grade: C";
+    }
+    else if (marks >= 60 && marks <= 79){
+        cout << "Grade: B";
+    }
+    else if (marks >= 80 && marks <=100){
+        cout << "Grade: A";
+    }
+    else{
+        cout << "Invalid marks entered." << endl;
+    }
+
+    return 0;
+} 
+*/
+
+#include <iostream>
+using namespace std;
+
+// Take the age from the user and decide accordingly:
+// 1. if age < 18:
+//     print -> not eligible for job
+// 2. if age>=18:
+//     print -> eligible for job
+// 3. if age >=55 and age <=57:
+//     print -> eligible for job, but retirement soon.
+// 4. if age > 57:
+//     print -> "retirement time"
+
+int main(){
+    int age;
+    cout << "Enter the age: ";
+    cin >> age;
+    if (age<18){
+        cout << "Not eligible for the job";
+    }
+    else if (age <= 57){
+        cout << "eligible for job";
+        if (age >= 55 && age <= 57){
+            cout << ", but retirement soon.";
+        }
+    }
+    else{
+        cout << "Retirement time";
+    }
+    
+
+    return 0;
+}
