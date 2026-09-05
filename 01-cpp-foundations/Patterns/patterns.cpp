d15f355d7484192acb617aad97a6ff3b69f83123
Eakempreet <eakempreetsinghlotey@gmail.com>
2026-09-06 01:02:30 +0530

    feat: pattern done 1-8

--- 01-cpp-foundations/Patterns/patterns.cpp
+++ 01-cpp-foundations/Patterns/patterns.cpp
@@ -84,6 +84,53 @@ void print5(int n){
         cout << endl;
     }
 }
+void print6(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+void print7(int n){
+    
+    for(int i=1; i<=n; i++){
+        //space
+        for(int j=1; j<=n-i; j++){
+            cout << " " << " ";
+        }
+        //star
+        for(int k=1; k<=2*i-1; k++){
+            cout << "* ";
+        }
+        //space
+        for(int l=1; l<=n-i; l++){
+            cout << " " << " ";
+        }
+        cout << endl;
+    }
+}
+void print8(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout << "  ";
+        }
+        //star
+        for(int k=0; k<2*n-(2*i+1); k++){
+            cout << "* ";
+        }
+        //space
+        for(int l=0; l<i; l++){
+            cout << "  ";
+        }
+        cout << endl;
+    }
+}
+void print9(int n){
+
+}
+
 
 int main(){
 
@@ -96,7 +143,7 @@ int main(){
         cout << "Enter: ";
         cin >> n;
 
-        print5(n);
+        print9(n);
 
     }
     
