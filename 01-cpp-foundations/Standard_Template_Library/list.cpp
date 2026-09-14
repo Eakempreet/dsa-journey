259029c828afb95e60bdb61e7688f9e3498b1695
Eakempreet <eakempreetsinghlotey@gmail.com>
2026-09-14 15:28:35 +0530

    feat: Basics of list container

---
+++ 01-cpp-foundations/Standard_Template_Library/list.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(){
+
+    list<int> ls;
+
+    ls.push_back(2); // {2}
+    ls.emplace_back(4); // {2, 4}
+
+    ls.push_front(5); // {5, 2, 4}
+    ls.emplace_front(); // {0, 5, 2, 4}
+    
+    for(auto it: ls){
+        cout << it << endl;
+    }
+
+    // rest functions same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+
+    return 0;
+}
